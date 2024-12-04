9a747b16d4c14e49daf7177b27a60bbd4942cdb4
nathanielodartey <nathanielodartey@gmail.com>
2024-12-03 22:10:33 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ import plotly.express as px
 st.header('Adverstisement of Cars')
 st.write('Filter the data below to see the ads by manufacturer')
 
- 
+
+car_advertisement = pd.read_csv('vehicles_us.csv')
